91d08f4bfa5225dfcd81e78d142db97e85cf6c29
Amy Parent <amy@amyparent.com>
2017-02-24 15:00:17 +0000

    Move ring buffer to its own header file

--- src/include/bin_export.h
+++ src/include/bin_export.h
@@ -2,58 +2,17 @@
 #define BIN_EXPORT_H
 
 #include <stdint.h>
-#include <stddef.h>
+#include "ring_buffer.h"
 
-template <typename T, size_t C>
-class ring_buffer {
+class bin_exporter {
+    
 public:
     
-    typedef T           value_type;
-    typedef T&          reference;
-    typedef const T&    const_reference;
+    bin_exporter();
+    ~bin_exporter();
     
-    /// Creates a new ring buffer.
-    ring_buffer()
-        : front_(0), back_(0) {}
+    void log(uint8_t data);
     
-    /// Returns the number of items available in the buffer.
-    size_t size() const {
-        return (C + front_ - back_) % C;
-    }
-    
-    /// Pushes [value] to the front of the buffer.
-    void push_front(const_reference value) {
-        size_t next_head = (front_ + 1) % C;
-        if(front_ == back_) { return; }
-        data_[front_] = value;
-        front_ = next_head;
-    }
-    
-    /// Consumes the item at the back of the buffer.
-    void pop_back() {
-        if(front_ == back_) { return; }
-        back_ = (back_ + 1) % C;
-    }
-    
-    /// Returns a constant reference to the next item available in the buffer.
-    const_reference back() const {
-        return data_[back_];
-    }
-    
-    /// Returns a the next item available in the buffer.
-    value_type cback() const {
-        return data_[back_];
-    }
-    
-    /// Clears the buffer.
-    void clear() {
-        front_ = back_ = 0;
-    }
-    
-private:
-    size_t              front_;
-    size_t              back_;
-    value_type          data_[C];
 };
 
 
